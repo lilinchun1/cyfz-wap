89aa2b819787ead1512cb8a6443872fdb05c6c62
llcly <45433256@163.com>
2014-09-29 05:49:14 +0800

    导航显示js ++

--- js/minjs.js
+++ js/minjs.js
@@ -6,33 +6,47 @@ window.onload = function () {
     var ulNb = navList1.getElementsByTagName("ul");
 
         ulNb[0].previousElementSibling.onclick = function () {
-            ulNb[0].style.display = "block";
             ulNb[1].style.display = "none";
             ulNb[2].style.display = "none";
             ulNb[3].style.display = "none";
+            if( ulNb[0].style.display == "block"){
+                ulNb[0].style.display = "none";
+            }else{
+
+                ulNb[0].style.display = "block";
+            }
         };
         ulNb[1].previousElementSibling.onclick = function () {
             ulNb[0].style.display = "none";
-            ulNb[1].style.display = "block";
             ulNb[2].style.display = "none";
             ulNb[3].style.display = "none";
+            if( ulNb[1].style.display == "block"){
+                ulNb[1].style.display = "none";
+            }else{
+
+                ulNb[1].style.display = "block";
+            }
         };
         ulNb[2].previousElementSibling.onclick = function () {
             ulNb[0].style.display = "none";
             ulNb[1].style.display = "none";
-            ulNb[2].style.display = "block";
             ulNb[3].style.display = "none";
+            if( ulNb[2].style.display == "block"){
+                ulNb[2].style.display = "none";
+            }else{
+                ulNb[2].style.display = "block";
+            }
         };
         ulNb[3].previousElementSibling.onclick = function () {
             ulNb[0].style.display = "none";
             ulNb[1].style.display = "none";
-            ulNb[2].style.display = "block";
-            ulNb[3].style.display = "none";
+            ulNb[2].style.display = "none";
+            if( ulNb[3].style.display == "block"){
+                ulNb[3].style.display = "none";
+            }else{
+
+                ulNb[3].style.display = "block";
+            }
         };
 
-    /*for( var i = 0; i < ulNb.length; i++){
-        ulNb[i].previousElementSibling.onclick = function () {
-            this.nextElementSibling.style.display = "block";
-        };
-    }*/
 };
