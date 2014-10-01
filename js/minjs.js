fe137b82181027433685f5f3c136404ffafdcb27
llcly <45433256@163.com>
2014-10-01 13:45:39 +0800

    ++

--- js/minjs.js
+++ js/minjs.js
@@ -4,49 +4,65 @@
 window.onload = function () {
     var navList1 = document.getElementById("J_nav_list1");
     var ulNb = navList1.getElementsByTagName("ul");
+    var oSpan = navList1.getElementsByTagName("span");
+    var logIn = document.getElementById("J_login");
 
-        ulNb[0].previousElementSibling.onclick = function () {
-            ulNb[1].style.display = "none";
-            ulNb[2].style.display = "none";
-            ulNb[3].style.display = "none";
-            if( ulNb[0].style.display == "block"){
-                ulNb[0].style.display = "none";
-            }else{
+    for(var i = 0; i < oSpan.length; i++){
 
-                ulNb[0].style.display = "block";
+        oSpan[i].num1 = i;
+        oSpan[i].onclick = function () {
+
+            for( var j = 0; j < oSpan.length; j++){
+
+                ulNb[j].style.display = "none";
             }
-        };
-        ulNb[1].previousElementSibling.onclick = function () {
+            ulNb[this.num1].style.display = "block";
+        }
+    }
+
+
+    /*ulNb[0].previousElementSibling.onclick = function () {
+        ulNb[1].style.display = "none";
+        ulNb[2].style.display = "none";
+        ulNb[3].style.display = "none";
+        if( ulNb[0].style.display == "block"){
             ulNb[0].style.display = "none";
-            ulNb[2].style.display = "none";
-            ulNb[3].style.display = "none";
-            if( ulNb[1].style.display == "block"){
-                ulNb[1].style.display = "none";
-            }else{
+        }else{
 
-                ulNb[1].style.display = "block";
-            }
-        };
-        ulNb[2].previousElementSibling.onclick = function () {
-            ulNb[0].style.display = "none";
+            ulNb[0].style.display = "block";
+        }
+    };
+    ulNb[1].previousElementSibling.onclick = function () {
+        ulNb[0].style.display = "none";
+        ulNb[2].style.display = "none";
+        ulNb[3].style.display = "none";
+        if( ulNb[1].style.display == "block"){
             ulNb[1].style.display = "none";
-            ulNb[3].style.display = "none";
-            if( ulNb[2].style.display == "block"){
-                ulNb[2].style.display = "none";
-            }else{
-                ulNb[2].style.display = "block";
-            }
-        };
-        ulNb[3].previousElementSibling.onclick = function () {
-            ulNb[0].style.display = "none";
-            ulNb[1].style.display = "none";
-            ulNb[2].style.display = "none";
-            if( ulNb[3].style.display == "block"){
-                ulNb[3].style.display = "none";
-            }else{
+        }else{
 
-                ulNb[3].style.display = "block";
-            }
-        };
+            ulNb[1].style.display = "block";
+        }
+    };
+    ulNb[2].previousElementSibling.onclick = function () {
+        ulNb[0].style.display = "none";
+        ulNb[1].style.display = "none";
+        ulNb[3].style.display = "none";
+        if( ulNb[2].style.display == "block"){
+            ulNb[2].style.display = "none";
+        }else{
+            ulNb[2].style.display = "block";
+        }
+    };
+    ulNb[3].previousElementSibling.onclick = function () {
+        ulNb[0].style.display = "none";
+        ulNb[1].style.display = "none";
+        ulNb[2].style.display = "none";
+        if( ulNb[3].style.display == "block"){
+            ulNb[3].style.display = "none";
+        }else{
+
+            ulNb[3].style.display = "block";
+        }
+    };*/
 
 };
