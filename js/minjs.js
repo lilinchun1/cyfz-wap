8538b078abe38f0febf1f83741ffbfcfc39348d6
llcly <45433256@163.com>
2014-10-01 15:27:14 +0800

    js++

--- js/minjs.js
+++ js/minjs.js
@@ -5,64 +5,36 @@ window.onload = function () {
     var navList1 = document.getElementById("J_nav_list1");
     var ulNb = navList1.getElementsByTagName("ul");
     var oSpan = navList1.getElementsByTagName("span");
-    var logIn = document.getElementById("J_login");
-
+//naver
     for(var i = 0; i < oSpan.length; i++){
 
         oSpan[i].num1 = i;
+        oSpan[i].num2 = 0;
         oSpan[i].onclick = function () {
 
             for( var j = 0; j < oSpan.length; j++){
-
                 ulNb[j].style.display = "none";
             }
-            ulNb[this.num1].style.display = "block";
+            switch (this.num2){
+
+                case 0:
+                    ulNb[this.num1].style.display = "block";
+                    for( var k = 0;  k < oSpan.length; k++){
+                        oSpan[k].num2 = 0;
+                    }
+                    this.num2 = 1;
+                    break;
+                case 1:
+                    ulNb[this.num1].style.display = "none";
+                    this.num2 = 0;
+                    break;
+            }
+
         }
     }
 
-
-    /*ulNb[0].previousElementSibling.onclick = function () {
-        ulNb[1].style.display = "none";
-        ulNb[2].style.display = "none";
-        ulNb[3].style.display = "none";
-        if( ulNb[0].style.display == "block"){
-            ulNb[0].style.display = "none";
-        }else{
-
-            ulNb[0].style.display = "block";
-        }
-    };
-    ulNb[1].previousElementSibling.onclick = function () {
-        ulNb[0].style.display = "none";
-        ulNb[2].style.display = "none";
-        ulNb[3].style.display = "none";
-        if( ulNb[1].style.display == "block"){
-            ulNb[1].style.display = "none";
-        }else{
-
-            ulNb[1].style.display = "block";
-        }
-    };
-    ulNb[2].previousElementSibling.onclick = function () {
-        ulNb[0].style.display = "none";
-        ulNb[1].style.display = "none";
-        ulNb[3].style.display = "none";
-        if( ulNb[2].style.display == "block"){
-            ulNb[2].style.display = "none";
-        }else{
-            ulNb[2].style.display = "block";
-        }
-    };
-    ulNb[3].previousElementSibling.onclick = function () {
-        ulNb[0].style.display = "none";
-        ulNb[1].style.display = "none";
-        ulNb[2].style.display = "none";
-        if( ulNb[3].style.display == "block"){
-            ulNb[3].style.display = "none";
-        }else{
-
-            ulNb[3].style.display = "block";
-        }
-    };*/
-
 };
+
+
+
+
